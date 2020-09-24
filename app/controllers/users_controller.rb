cbc95ef6eefaf5305159f34c4d9fc7a9cc8ea11b
OrkunSA <c.orkun.s@gmail.com>
2020-09-24 17:05:13 -0500

    update application/users controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+    helper_method :logged_in?, :current_user
+    layout 'static'
+
+    def new 
+        @user = User.new 
+    end
+
+    def create 
+        @user = User.new 
+        if @user.valid? 
+            session[:user_id] = @user.id 
+            redirect_to user_movieboxes_path(@user)
+        else
+            render :new 
+        end
+    end
+
+    
+
+
+
 end
