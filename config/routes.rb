aa85e5d81d7c5195daaf7f163852abc6ac3638ac
OrkunSA <c.orkun.s@gmail.com>
2020-09-24 16:54:33 -0500

    add login/logut path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  
   resources :movieboxes
   resources :users
   resources :movies
   resources :movie_genres
   resources :listings
   resources :genres
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
