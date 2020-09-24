f5559ab31fb08b5d7b0c7923e8cff8e2bb8435ab
OrkunSA <c.orkun.s@gmail.com>
2020-09-24 16:58:53 -0500

    add helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in? 
+
+    def current_user 
+        @user = User.find_by(id: session[:user_id)
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def require_login
+        unless logged_in? 
+            flash[:danger] = "Oh huh! Looks like you don't have enough power to view this section"
+            redirect_to login_path 
+        end
+    end
+
 end
