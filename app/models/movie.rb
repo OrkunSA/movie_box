546965774b0ba4a3c51059b4b22f4753bab8bc6f
OrkunSA <c.orkun.s@gmail.com>
2020-09-25 10:29:17 -0500

    update movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,5 +4,17 @@ class Movie < ApplicationRecord
     has_many :users, through: :listings 
     has_many :movie_genres
     has_many :genres, through: :movie_genres
+
+    def genres_attributes=(genre_attributes)
+        genre_name = genre_attributes["0"]["name"]
+        unless genre_name.blank?
+          genre = Genre.find_or_create_by(name: genre_name)
+          self.genres << genre
+        end
+    end
+
+    def movies_movieboxes_by_user(user_id)
+        self.listings.where(user_id: user_id)
+    end
     
 end
