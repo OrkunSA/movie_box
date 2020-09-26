7ea86d230110c9046bcd9c91e188e7ccac90367b
OrkunSA <c.orkun.s@gmail.com>
2020-09-26 14:28:35 -0500

    update listings  controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,54 @@
 class ListingsController < ApplicationController
+    before_action :require_login
+    before_action :set_listing, only: [:edit, :update, :destroy]
+
+    def new 
+        @listing = Listing.new 
+        @listing.movie = Movie.find_by(id: params[:movie_id])
+    end
+
+    def create 
+        @listing = Listing.new(listing_params)
+        @listing.movie = Movie.find_by(id: params[:movie_id])
+        if @listing.save
+            flash[:message] = "#{@listing.movie.title} has been added to your watchlist"
+            redirect_to movie_path(@listing.movie)
+        else
+            render :new 
+        end
+    end
+
+    def edit 
+    end
+
+    def update 
+        @listing.movie(listing_params)
+        if @listing.save
+            flash[:message] = "#{@listing.movie.title} listing has been updated"
+            redirect_to movie_path(@listing.movie)
+        else
+           render :edit 
+        end
+    end
+
+    def destroy 
+        if @listing
+            @listing.destroy
+            flash[:message] = "The movie has been removed from your moviebox"
+            redirect_to movieboxes_path
+        else
+            flash[:danger] = "The movie could not be removed"
+            render 'movies/show'
+        end
+    end
+
+    private 
+    def listing_params
+        params.require(:listing).permit(:moviebox_id, :user_id, :user_status, :favorite)
+    end
+
+    def set_listing 
+        @listing = Listing.find_by(id: params[:id])
+    end
+
 end
