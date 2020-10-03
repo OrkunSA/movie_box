5c7084c33e134eac955cfc45f8c60509ce5025e3
OrkunSA <c.orkun.s@gmail.com>
2020-10-03 15:52:24 -0500

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
     has_many :movieboxes
     has_many :listings 
     has_many :movies, through: :listings
-
-    
+    validates :email, presence: true 
+    validates :username, presence: true 
     has_secure_password
 
     def favorite_shows
-        # List the user's shows where fav is true
+        # List the user's movies where favorite is true
         self.movies.includes(:listings).where(:listings => { favorite: true }).uniq
     end
 end
